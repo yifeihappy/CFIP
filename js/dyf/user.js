755c6c459740b75016f1287d42b1d4be17adb536
yifeihappy <yifeihappy@YIFEIHAPPY-PC>
2017-12-17 15:20:50 +0800

    添加密码验证功能

--- js/dyf/user.js
+++ js/dyf/user.js
@@ -1,4 +1,47 @@
-function userSignIn() {
-	console.log($("#password ").val());
-	console.log($("#studentNumber ").val());
+function getByStudentNumber(studentNumber) {
+	var student;
+	$.ajax({
+		type: "get",
+		url: "http://112.74.35.75:8080/Entity/U3d616b41047817/CFIP/Student/?Student.studentnumber=" + studentNumber,
+		async: false,
+		success: function(data, state) {
+			console.log(state);
+			console.log(data);
+			console.log(data.Student[0].studentname);
+			student = data.Student[0];
+			//return data;
+		}
+	});
+	return student;
+}
+
+function getByTeacherNumber(teacherNumber) {
+	var teacher;
+	$.ajax({
+		type: "get",
+		url: "http://112.74.35.75:8080/Entity/U3d616b41047817/CFIP/Student/?Student.studentnumber=" + studentNumber,
+		async: true,
+		success: function(data, state) {
+			console.log(state);
+			console.log(data);
+			teacher = data.Teacher[0];
+		}
+	});
+	return teacher;
+}
+
+function getByAdminNumber(adminNumber) {
+	var admin;
+	$.ajax({
+		type: "get",
+		url: "http://112.74.35.75:8080/Entity/U3d616b41047817/CFIP/Student/?Student.studentnumber=" + studentNumber,
+		async: true,
+		success: function(data, state) {
+			console.log(state);
+			console.log(data);
+			admin = data.Admin[0];
+			return data;
+		}
+	});
+	return admin;
 }
