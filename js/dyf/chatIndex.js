d70bab0f00b9e06a51c2bf1743d989bdcb6fd0bf
yifeihappy <yifeihappy@YIFEIHAPPY-PC>
2017-12-23 19:22:25 +0800

    jh

--- js/dyf/chatIndex.js
+++ js/dyf/chatIndex.js
@@ -223,17 +223,17 @@ function createYVedioItem(userID, currentTime1, Icontent, msgID) {
 	//$("#allMessagePlane").append(divRow);
 	return divRow;
 }
-//获取当前时间
-function getFormatDate() {
-	var nowDate = new Date();
-	var year = nowDate.getFullYear();
-	var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1;
-	var date = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
-	var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
-	var minute = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
-	//var second = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
-	return year + "-" + month + "-" + date + "T" + hour + ":" + minute;
-}
+////获取当前时间
+//function getFormatDate() {
+//	var nowDate = new Date();
+//	var year = nowDate.getFullYear();
+//	var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1;
+//	var date = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
+//	var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+//	var minute = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+//	//var second = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+//	return year + "-" + month + "-" + date + "T" + hour + ":" + minute;
+//}
 
 //监听有无新的消息
 function listenToMessageTrigger() {
--- js/dyf/user.js
+++ js/dyf/user.js
@@ -174,6 +174,17 @@ function IsHasNewMessage() {
 	return flagF;
 }
 
+//获取当前时间
+function getFormatDate() {
+	var nowDate = new Date();
+	var year = nowDate.getFullYear();
+	var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1;
+	var date = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
+	var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+	var minute = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+	//var second = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+	return year + "-" + month + "-" + date + "T" + hour + ":" + minute;
+}
 //var userID = "";
 //$(document).ready(function() {
 //	var urlParam = window.location.href;
